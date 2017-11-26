b75f8badc59eab6ec3e89300b76f03b457652493
wenbegn <wenbegn@wenbegndeimac.local>
2017-11-26 21:42:39 +0800

    Initial Commit

---
+++ MenuTableList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MenuTableList
+//
+//  Created by wenbegn on 2017/11/26.
+//  Copyright © 2017年 wenbegn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MenuTableList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MenuTableList
+//
+//  Created by wenbegn on 2017/11/26.
+//  Copyright © 2017年 wenbegn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
